1cec34238c0b9b80f7ed3bb99e73ca559ec6baf7
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-06-13 23:09:48 +0200

    Add initActions

--- src/js/script.js
+++ src/js/script.js
@@ -7,14 +7,16 @@
       product: '#template-book',
     },
     containerOf: {
-      product: '.books-list'
+      product: '.books-list',
+      productImage: '.books-list .book__image'
     }
   };
   const templates = {
     product: Handlebars.compile(document.querySelector(select.templateOf.product).innerHTML),
   };
   const data = dataSource;
-  //console.log(dataSource);
+
+  const favouriteBooks = [];
   
   function render(){
     for (let productData in data.books){
@@ -35,7 +37,44 @@
       productContainer.appendChild(generatedDOM);
     }
   }
+
+  function initActions(){
+    const bookImages = document.querySelectorAll(select.containerOf.productImage);
+    //console.log(bookImages);
+
+    for (const bookImage of bookImages){
+      //console.log(bookImage);
+
+      bookImage.addEventListener('click', function(event){
+        event.preventDefault();
+      });
+      
+      bookImage.addEventListener('dblclick', function(event){
+        event.preventDefault();
+
+        let id = bookImage.getAttribute('data-id');
+
+        //if(favouriteBooks[id]){
+        if(bookImage.classList.contains('favorite')){
+
+          bookImage.classList.remove('favorite');
+
+          const indexOfBook = favouriteBooks.indexOf(id);
+          favouriteBooks.splice(indexOfBook,1);
+        } else {
+          bookImage.classList.add('favorite');
+          favouriteBooks.push(id);
+        }
+        console.log(favouriteBooks);
+      });
+
+
+    }
+
+  }
+  
   
   
   render();
+  initActions();
 }
